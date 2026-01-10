7d8aa6012c19db6f2b41acba9e4f609434a53a82
Bohdan <bodia02981@gmail.com>
2026-01-10 19:47:41 +0200

    Finalized parsing all types of data for .torrent file

--- main.c
+++ main.c
@@ -12,35 +12,35 @@
 #define DIGITS_IN_LONG 19
 
 typedef struct {
-    const char* key;
-    const char* value;
+    const char *key;
+    const char *value;
 } DictionaryItem;
 
 const DictionaryItem BencodeSymbols[] = {
     {"d", "e"}
 };
 
-const char* bencodeSymbols[] = {"d", "l"};
+const char *bencodeSymbols[] = {"d", "l"};
 
-char getAsciiCharFromBinary(const char* const str) {
+char getAsciiCharFromBinary(const char *const str) {
     int multiplier = 1;
 
-    return (char)str[0];
+    return (char) str[0];
 }
 
 typedef struct TorrentInfo {
-    char* fileName;
+    char *fileName;
     int fileSize;
 } TorrentInfo;
 
 
 struct Torrent {
-    char* announce;
-    char** announceList;
+    char *announce;
+    char **announceList;
     int announceListSize;
 
-    char* comment;
-    char* createdBy;
+    char *comment;
+    char *createdBy;
     int creationDateTimestamp;
     TorrentInfo infos[];
 } Torrent;
@@ -49,12 +49,118 @@ bool isDigit(const int ch) {
     return ch >= 48 && ch <= 57;
 }
 
-int fpeek(FILE * const fp) {
+int fpeek(FILE *const fp) {
     const int c = fgetc(fp);
     if (c != EOF) {
         ungetc(c, fp);
     }
     return c;
+}
+
+// TODO: handle edge cases https://en.wikipedia.org/wiki/Bencode
+
+void getNumberFromTorrentFile(int* ch, FILE *const torrentFile, bool *isSuccess, long* number) {
+    char numbersBuffer[DIGITS_IN_LONG + 1];
+    int position = 0;
+    numbersBuffer[position++] = (char) *ch;
+    while ((*ch = fgetc(torrentFile)) != EOF && isDigit(*ch) && position < DIGITS_IN_LONG) {
+        numbersBuffer[position++] = (char) *ch;
+    }
+
+    numbersBuffer[position] = '\0';
+
+    if (position > DIGITS_IN_LONG) {
+        *isSuccess = false;
+        return;
+    }
+
+    *number = strtol(numbersBuffer, NULL, 10);
+}
+
+void handleDigit(int *ch, FILE *const torrentFile, bool *isSuccess) {
+    long charsToRead;
+    getNumberFromTorrentFile(ch, torrentFile, isSuccess, &charsToRead);
+
+    if (!*isSuccess) {
+        return;
+    }
+    if (*ch != COLUMN_CHAR) {
+        *isSuccess = false;
+        return;
+    }
+
+    char stringBuffer[charsToRead + 1];
+    fread(stringBuffer, sizeof(char), charsToRead, torrentFile);
+    stringBuffer[charsToRead] = '\0';
+    printf("\nReading %ld chars \n", charsToRead);
+    printf("%s", stringBuffer);
+}
+
+void handleInt(int *ch, FILE *const torrentFile, bool *isSuccess) {
+    long parsedNumber;
+    getNumberFromTorrentFile(ch, torrentFile, isSuccess, &parsedNumber);
+    printf("\n%ld\n", parsedNumber);
+}
+
+void handleList(int *ch, FILE *const torrentFile, bool *isSuccess) {
+    while ((*ch = fgetc(torrentFile)) != EOF && *ch != END_CHAR) {
+        if (isDigit(*ch)) {
+            handleDigit(ch, torrentFile, isSuccess);
+            if (!*isSuccess) {
+                return;
+            }
+        } else if (*ch == DICT_CHAR) {
+            printf("OHHHHHHH DICT IN LIST");
+            // handleDict(ch, torrentFile, isSuccess);
+            // if (!*isSuccess) {
+            //     return;
+            // }
+        } else if (*ch == INT_CHAR) {
+            handleInt(ch, torrentFile, isSuccess);
+            if (!*isSuccess) {
+                return;
+            }
+        } else if (*ch == LIST_CHAR) {
+            handleList(ch, torrentFile, isSuccess);
+        } else {
+            *isSuccess = false;
+            return;
+        }
+    }
+
+    if (*ch == EOF) {
+        *isSuccess = false;
+    }
+}
+
+void handleDict(int *ch, FILE *const torrentFile, bool *isSuccess) {
+    while ((*ch = fgetc(torrentFile)) != EOF && *ch != END_CHAR) {
+        if (isDigit(*ch)) {
+            handleDigit(ch, torrentFile, isSuccess);
+            if (!*isSuccess) {
+                return;
+            }
+        } else if (*ch == DICT_CHAR) {
+            handleDict(ch, torrentFile, isSuccess);
+            if (!*isSuccess) {
+                return;
+            }
+        } else if (*ch == INT_CHAR) {
+            handleInt(ch, torrentFile, isSuccess);
+            if (!*isSuccess) {
+                return;
+            }
+        } else if (*ch == LIST_CHAR) {
+            handleList(ch, torrentFile, isSuccess);
+        } else {
+            *isSuccess = false;
+            return;
+        }
+    }
+
+    if (*ch == EOF) {
+        *isSuccess = false;
+    }
 }
 
 int main() {
@@ -76,36 +182,11 @@ int main() {
         exit(-1);
     }
 
-    while ((ch = fgetc(torrentFile)) != EOF && ch != END_CHAR) {
-        if (isDigit(ch)) {
-            char numbersBuffer[DIGITS_IN_LONG + 1];
-            int position = 0;
-            numbersBuffer[position++] = (char)ch;
-            while ((ch = fgetc(torrentFile)) != EOF && isDigit(ch) && position < DIGITS_IN_LONG) {
-                numbersBuffer[position++] = (char)ch;
-            }
-
-            numbersBuffer[position] = '\0';
-
-            if (ch != COLUMN_CHAR || position > DIGITS_IN_LONG) {
-                fclose(torrentFile);
-                exit(-1);
-            }
-
-            const long charsToRead = strtol(numbersBuffer, NULL, 10);
-            char stringBuffer[charsToRead + 1];
-            fread(stringBuffer, sizeof(char), charsToRead, torrentFile);
-            stringBuffer[charsToRead] = '\0';
-            printf("\n Reading %ld chars \n", charsToRead);
-            printf("%s", stringBuffer);
-        } else if (ch == DICT_CHAR) {
-        } else if (ch == INT_CHAR) {
-        } else if (ch == LIST_CHAR) {
-        } else {
-            exit(-1);
-        }
+    bool isSuccess = true;
+    handleDict(&ch, torrentFile, &isSuccess);
+    if (!isSuccess) {
+        return -1;
     }
-
     if (ch == EOF) {
         fclose(torrentFile);
         exit(-1); // TODO: Error: dictionary was not closed by e
