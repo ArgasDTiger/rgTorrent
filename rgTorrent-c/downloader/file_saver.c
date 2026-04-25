ee2470b13cf99d051a54d96611465dc145eb1423
Bohdan <bodia02981@gmail.com>
2026-04-26 00:04:39 +0300

    Added function to create parent dirs when downloading a torrent fiel

--- rgTorrent-c/downloader/file_saver.c
+++ rgTorrent-c/downloader/file_saver.c
@@ -3,6 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+
+void create_parent_directories(const char *filepath) {
+    char temp_path[1024];
+    strncpy(temp_path, filepath, sizeof(temp_path) - 1);
+    temp_path[sizeof(temp_path) - 1] = '\0';
+
+    char *last_slash = strrchr(temp_path, '/');
+    if (!last_slash) return;
+
+    *last_slash = '\0';
+
+    char *p = temp_path;
+    if (*p == '/') p++;
+
+    for (; *p; p++) {
+        if (*p == '/') {
+            *p = '\0';
+            mkdir(temp_path, 0777);
+            *p = '/';
+        }
+    }
+    mkdir(temp_path, 0777);
+}
 
 void write_piece_to_disk(const uint32_t piece_index, const size_t piece_length, const unsigned char *piece_buffer,
                          const EndFile *end_files, const int num_files) {
@@ -25,6 +49,7 @@ void write_piece_to_disk(const uint32_t piece_index, const size_t piece_length, 
         const size_t write_length = overlap_end - overlap_start;
         const size_t local_file_offset = overlap_start - file->global_start;
 
+        create_parent_directories(file->filepath);
         FILE *f = fopen(file->filepath, "rb+");
         if (!f) {
             f = fopen(file->filepath, "wb+");
@@ -32,9 +57,10 @@ void write_piece_to_disk(const uint32_t piece_index, const size_t piece_length, 
 
         if (f) {
             fseek(f, local_file_offset, SEEK_SET);
-
             fwrite(piece_buffer + bytes_written_from_buffer, 1, write_length, f);
             fclose(f);
+        } else {
+            fprintf(stderr, "[ERROR] Could not open/create file when writing to the disk: %s.\n", file->filepath);
         }
 
         bytes_written_from_buffer += write_length;
